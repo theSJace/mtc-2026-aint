a16dfffd28f6520a7be49b2878806e1e67d807e6
bivanalhar <bivan.alzacky@gmail.com>
2026-03-07 15:29:45 +0800

    project setup

---
+++ convex/schema.ts
@@ -0,0 +1,8 @@
+import { defineSchema } from "convex/server";
+
+/**
+ * Convex schema.
+ * Add tables and fields here for your app.
+ * @see https://docs.convex.dev/database/schemas
+ */
+export default defineSchema({});
